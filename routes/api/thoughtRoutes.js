6a0864769b4fcee943a5228404b53ae7a65b7cff
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-04-10 20:03:06 -0700

    added thoughts routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,17 @@ const {
     createReaction, 
     deleteReaction,
 } = require('../../controllers');
+
+//GET all thoughts /CREATE thoughts api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// GET a single thought / UPDATE a thought / DELETE a thought api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+//  CREATE reaction api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+//  DELETE reaction api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions').delete(deleteReaction);
+
+module.exports = router;
